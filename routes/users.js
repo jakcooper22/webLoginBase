646e6f91e41df4e690b3a4ee17b1c67e410b69d8
jake <notNow@notNow.com>
2021-07-31 16:42:40 -0500

    adding sql

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,31 @@
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('usersTable');
+//var db = new sqlite3.Database('../newTableTest');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/users', function(req, res, next) {
-  //console.log('got a request for a user');
-	res.send('got a user req');
-	console.log('got a user req');
-	console.log(req.body);
-	  
+  var db = new sqlite3.Database("../newTableTest");
+  db.run('Create table usersTable(userId int, username varchar, password varchar)', (error) => {
+    if (error) {
+      db.run('insert into usersTable(userId, username, password) values(?,?,?)',
+              [1234, req.body.uname, req.body.pword], (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('inserted new user into table');  
+          db.close();
+        }
+      });
+    } else {
+      console.log('table created');
+    };
+  });
 });
+
+function initDB() {
+  //db
+
+}
 
 module.exports = router;
