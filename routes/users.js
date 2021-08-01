56224cb83dda69385be63d3efa34bfb4a93f45cd
jake <notNow@notNow.com>
2021-08-01 16:29:56 -0500

    setting up user login in sqlite3 and creating jwt

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,59 @@
 var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
 //var db = new sqlite3.Database('../newTableTest');
+var md5 = require('md5');
+var jwt = require('jsonwebtoken');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/users', function(req, res, next) {
-  var db = new sqlite3.Database("../newTableTest");
+  var db = new sqlite3.Database("db/newTableTest");
   db.run('Create table usersTable(userId int, username varchar, password varchar)', (error) => {
     if (error) {
       db.run('insert into usersTable(userId, username, password) values(?,?,?)',
-              [1234, req.body.uname, req.body.pword], (err) => {
+              [1234, req.body.uname, md5(req.body.pword)], (err) => {
         if (err) {
           console.log(err);
         } else {
-          console.log('inserted new user into table');  
+          console.log('inserted new user into table and db is already created');  
           db.close();
+          const token = jwt.sign({"uname":req.body.uname, 
+                                  "pword":md5(req.body.pword)},
+                                  "piauwhakjsdnfakjsdhf34980745ljkhaaf",
+                                 {
+                                   expiresIn: "2h"
+                                 });
+          const decodedToken = jwt.verify(token, "piauwhakjsdnfakjsdhf34980745ljkhaaf");
+          res.json({
+            "message" : "success",
+            "token" : token,
+            "decodedToken" : decodedToken
+          });
         }
       });
     } else {
       console.log('table created');
+      db.run('insert into usersTable(userId, username, password) values(?,?,?)',
+              [1234, req.body.uname, md5(req.body.pword)], (err) => {
+        if (err) {
+          console.log(err);
+          res.json({
+            "message" : "error",
+            "data" : {
+              "username" : req.body.uname,
+              "pword" : req.body.pword
+            }
+          });
+        } else {
+          console.log('inserted new user into table and db is not created');  
+          res.json({
+            "message" : "user inserted"
+          });
+          db.close();          
+        }
+      });
     };
   });
 });
-
-function initDB() {
-  //db
-
-}
 
 module.exports = router;
--- testSql/testSqlCon.js
+++ testSql/testSqlCon.js
@@ -1,10 +1,19 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('../user.db');
 
-db.serialize(() => {
-	db.each('SELECT userId, username, password from usersTable', function (err, row) {
-		console.log('User: ', row.userId, row.username, row.password);
-	});
-});
+function test1() {
+  db.serialize(() => {
+    db.each('SELECT userId, username, password from usersTable', function (err, row) {
+      console.log('User: ', row.userId, row.username, row.password);
+    });
+  });
+};
+ 
+function test2(){
+  db.run('insert into usersTable(userId, username, password) values(?,?,?)',
+         [123, 'test2', 'test2']);
+};
+
+test2();
 
 db.close();
